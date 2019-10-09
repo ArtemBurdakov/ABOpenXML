3c4d1b848326fe4b2defdaf9e331c52444559fbf
ArtemBurdakov <55960599+ArtemBurdakov@users.noreply.github.com>
2019-10-09 16:05:13 +0300

    Create IDefaultContentType.vb

---
+++ ContentType/IDefaultContentType.vb
@@ -0,0 +1,14 @@
+Namespace ContentType
+
+    ''' <summary>
+    ''' Интерфейс стандартного типа содержимого в формате OfficeOpenXML.
+    ''' </summary>
+    Public Interface IDefaultContentType
+
+        Property Extension As String
+        Property ContentType As String
+
+    End Interface
+
+End Namespace
+
