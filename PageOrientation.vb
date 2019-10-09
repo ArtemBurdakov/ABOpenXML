fbb3283bf11a7f0ca06baa4d050c4a5b4511a875
ArtemBurdakov <55960599+ArtemBurdakov@users.noreply.github.com>
2019-10-09 14:33:21 +0300

    Create PageOrientation.vb

---
+++ PageOrientation.vb
@@ -0,0 +1,17 @@
+''' <summary>
+''' Варианты ориентации страницы при печати для формата OfficeOpenXML.
+''' </summary>
+Public Class PageOrientation
+
+    ''' <summary>
+    ''' Портеретная ориентация.
+    ''' </summary>
+    Public Shared Property Portrait As String = "portrait"
+
+    ''' <summary>
+    ''' Альбомная ориентация.dsvsdvsvsdvsvsdvsvsd
+    ''' </summary>
+    Public Shared Property Landscape As String = "landscape"
+
+End Class
+
