1b270e2bfada3d64e1c58df148b1291ee4a2e51d
ArtemBurdakov <55960599+ArtemBurdakov@users.noreply.github.com>
2019-10-09 16:42:30 +0300

    Create NS.vb

---
+++ XML/NS.vb
@@ -0,0 +1,18 @@
+Namespace XML
+
+    ''' <summary>
+    ''' Пространства имён для формата OfficeOpenXML.
+    ''' </summary>
+    Class NS
+        Shared Property ct As XNamespace = "http://schemas.openxmlformats.org/package/2006/content-types"
+        Shared Property rs As XNamespace = "http://schemas.openxmlformats.org/package/2006/relationships"
+        Shared Property wb As XNamespace = "http://schemas.openxmlformats.org/spreadsheetml/2006/main"
+        Shared Property r As XNamespace = "http://schemas.openxmlformats.org/officeDocument/2006/relationships"
+        Shared Property mc As XNamespace = "http://schemas.openxmlformats.org/markup-compatibility/2006"
+        Shared Property x14ac As XNamespace = "http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac"
+        Shared Property xdr As XNamespace = "http://schemas.openxmlformats.org/drawingml/2006/spreadsheetDrawing"
+        Shared Property a As XNamespace = "http://schemas.openxmlformats.org/drawingml/2006/main"
+    End Class
+
+End Namespace
+
