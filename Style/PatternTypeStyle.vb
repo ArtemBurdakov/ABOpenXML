40e0c876d12dff830003f36edfba7ff70a1a40f9
ArtemBurdakov <55960599+ArtemBurdakov@users.noreply.github.com>
2019-10-09 16:53:46 +0300

    Create PatternTypeStyle.vb

---
+++ Style/PatternTypeStyle.vb
@@ -0,0 +1,106 @@
+Namespace Style
+
+    ''' <summary>
+    ''' Варианты узора заливки ячейки в Excel для формата OfficeOpenXML.
+    ''' </summary>
+    Public Class PatternTypeStyle
+
+        ''' <summary>
+        ''' Стиль заливки - dark down.
+        ''' </summary>
+        Public Shared Property DarkDown As String = "darkDown"
+
+        ''' <summary>
+        ''' Стиль заливки - dark gray.
+        ''' </summary>
+        Public Shared Property DarkGray As String = "darkGray"
+
+        ''' <summary>
+        ''' Стиль заливки - dark grid.
+        ''' </summary>
+        Public Shared Property DarkGrid As String = "darkGrid"
+
+        ''' <summary>
+        ''' Стиль заливки - dark horizontal.
+        ''' </summary>
+        Public Shared Property DarkHorizontal As String = "darkHorizontal"
+
+        ''' <summary>
+        ''' Стиль заливки - dark trellis.
+        ''' </summary>
+        Public Shared Property DarkTrellis As String = "darkTrellis"
+
+        ''' <summary>
+        ''' Стиль заливки - dark up.
+        ''' </summary>
+        Public Shared Property DarkUp As String = "darkUp"
+
+        ''' <summary>
+        ''' Стиль заливки - dark vertical.
+        ''' </summary>
+        Public Shared Property DarkVertical As String = "darkVertical"
+
+        ''' <summary>
+        ''' Стиль заливки - gray0625.
+        ''' </summary>
+        Public Shared Property Gray0625 As String = "gray0625"
+
+        ''' <summary>
+        ''' Стиль заливки - gray125.
+        ''' </summary>
+        Public Shared Property Gray125 As String = "gray125"
+
+        ''' <summary>
+        ''' Стиль заливки - light down.
+        ''' </summary>
+        Public Shared Property LightDown As String = "lightDown"
+
+        ''' <summary>
+        ''' Стиль заливки - light gray.
+        ''' </summary>
+        Public Shared Property LightGray As String = "lightGray"
+
+        ''' <summary>
+        ''' Стиль заливки - light grid.
+        ''' </summary>
+        Public Shared Property LightGrid As String = "lightGrid"
+
+        ''' <summary>
+        ''' Стиль заливки - light horizontal.
+        ''' </summary>
+        Public Shared Property LightHorizontal As String = "lightHorizontal"
+
+        ''' <summary>
+        ''' Стиль заливки - light trellis.
+        ''' </summary>
+        Public Shared Property LightTrellis As String = "lightTrellis"
+
+        ''' <summary>
+        ''' Стиль заливки - light up.
+        ''' </summary>
+        Public Shared Property LightUp As String = "lightUp"
+
+        ''' <summary>
+        ''' Стиль заливки - light vertical.
+        ''' </summary>
+        Public Shared Property LightVertical As String = "lightVertical"
+
+        ''' <summary>
+        ''' Стиль заливки - medium gray.
+        ''' </summary>
+        Public Shared Property MediumGray As String = "mediumGray"
+
+        ''' <summary>
+        ''' Стиль заливки - none.
+        ''' </summary>
+        Public Shared Property None As String = "none"
+
+        ''' <summary>
+        ''' Стиль заливки - solid.
+        ''' </summary>
+        Public Shared Property Solid As String = "solid"
+
+    End Class
+
+End Namespace
+
