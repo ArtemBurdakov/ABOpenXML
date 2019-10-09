e662eedd46018fb2591a48a2f5329f96c2379609
ArtemBurdakov <55960599+ArtemBurdakov@users.noreply.github.com>
2019-10-09 16:49:27 +0300

    Create BorderLineStyles.vb

---
+++ Style/BorderLineStyles.vb
@@ -0,0 +1,81 @@
+Namespace Style
+
+    ''' <summary>
+    ''' Варианты стиля ганицы ячейки в Excel для формата OfficeOpenXML.
+    ''' </summary>
+    Public Class BorderLineStyles
+
+        ''' <summary>
+        ''' Стиль границы Dash Dot.
+        ''' </summary>
+        Public Shared Property DashDot As String = "dashDot"
+
+        ''' <summary>
+        ''' Стиль границы Dash Dot Dot.
+        ''' </summary>
+        Public Shared Property DashDotDot As String = "dashDotDot"
+
+        ''' <summary>
+        ''' Стиль границы Dashed.
+        ''' </summary>
+        Public Shared Property Dashed As String = "dashed"
+
+        ''' <summary>
+        ''' Стиль границы Dotted.
+        ''' </summary>
+        Public Shared Property Dotted As String = "dotted"
+
+        ''' <summary>
+        ''' Стиль границы Double.
+        ''' </summary>
+        Public Shared Property DoubleLine As String = "double"
+
+        ''' <summary>
+        ''' Стиль границы Hair.
+        ''' </summary>
+        Public Shared Property Hair As String = "hair"
+
+        ''' <summary>
+        ''' Стиль границы Medium.
+        ''' </summary>
+        Public Shared Property Medium As String = "medium"
+
+        ''' <summary>
+        ''' Стиль границы Medium Dash Dot.
+        ''' </summary>
+        Public Shared Property MediumDashDot As String = "mediumDashDot"
+
+        ''' <summary>
+        ''' Стиль границы Medium Dash Dot Dot.
+        ''' </summary>
+        Public Shared Property MediumDashDotDot As String = "mediumDashDotDot"
+
+        ''' <summary>
+        ''' Стиль границы Medium Dashed.
+        ''' </summary>
+        Public Shared Property MediumDashed As String = "mediumDashed"
+
+        ''' <summary>
+        ''' Стиль границы None.
+        ''' </summary>
+        Public Shared Property None As String = "none"
+
+        ''' <summary>
+        ''' Стиль границы Slant Dash Dot.
+        ''' </summary>
+        Public Shared Property SlantDashDot As String = "slantDashDot"
+
+        ''' <summary>
+        ''' Стиль границы Thick.
+        ''' </summary>
+        Public Shared Property Thick As String = "thick"
+
+        ''' <summary>
+        ''' Стиль границы Thin.
+        ''' </summary>
+        Public Shared Property Thin As String = "thin"
+
+    End Class
+
+End Namespace
+
