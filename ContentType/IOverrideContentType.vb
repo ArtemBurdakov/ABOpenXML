efc04601ec90a2e38c3ca2fdfdedf0fef6b1fc66
ArtemBurdakov <55960599+ArtemBurdakov@users.noreply.github.com>
2019-10-09 16:06:20 +0300

    Create IOverrideContentType.vb

---
+++ ContentType/IOverrideContentType.vb
@@ -0,0 +1,15 @@
+Namespace ContentType
+
+    ''' <summary>
+    ''' Интерфейс компонента пакета в формате OfficeOpenXML.
+    ''' </summary>
+    Public Interface IOverrideContentType
+
+        Property PartName As String
+        Property ContentType As String
+        Property Unique As Boolean
+
+    End Interface
+
+End Namespace
+
